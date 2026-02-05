000fd6b1c5d0780188af5429f28f92b5fca3c7af
Ljubica Djoric <ljubica.djoric@yahoo.com>
2026-02-05 11:07:12 -0800

    feat: load posts dynamically from API

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../api";
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/posts`)
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Greška pri učitavanju postova:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Učitavanje postova...</p>;
+  }
+
   return (
     <div>
       <h1>📄 Posts</h1>
 
       <ul>
-        <li>
-          <Link to="/posts/1">Post 1</Link>
-        </li>
-        <li>
-          <Link to="/posts/2">Post 2</Link>
-        </li>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
