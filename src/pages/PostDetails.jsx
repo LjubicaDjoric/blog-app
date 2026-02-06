00757a77028843d68931ba7eee4508647888c0f8
Ljubica Djoric <ljubica.djoric@yahoo.com>
2026-02-06 11:12:46 -0800

    feat: post details page

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,45 +1,29 @@
-import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { API_URL } from "../api";
 
 function PostDetails() {
   const { id } = useParams();
   const [post, setPost] = useState(null);
-  const [error, setError] = useState("");
 
   useEffect(() => {
-    async function fetchPost() {
-      try {
-        setError("");
-        const res = await axios.get(`${API_URL}/posts/${id}`);
-        setPost(res.data);
-      } catch (err) {
-        setError("Post nije pronađen ili server nije pokrenut.");
-      }
-    }
-
-    fetchPost();
+    axios
+      .get(`${API_URL}/posts/${id}`)
+      .then((res) => setPost(res.data))
+      .catch((err) => console.error(err));
   }, [id]);
 
-  if (error) {
-    return (
-      <div>
-        <p style={{ color: "red" }}>{error}</p>
-        <Link to="/posts">← Nazad na listu</Link>
-      </div>
-    );
+  if (!post) {
+    return <p>Učitavanje...</p>;
   }
-
-  if (!post) return <p>Učitavanje...</p>;
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
 
-      <br />
-      <Link to="/posts">← Nazad na listu</Link>
+      <Link to="/posts">← Nazad na postove</Link>
     </div>
   );
 }
